b4249479b215f5d2a336f21c57979cb87616eae4
dsemeraro2 <daniele.semerarods@libero.it>
2025-04-12 15:03:51 +0200

    Upgrade Angular and related dependencies to version 18.x, update TypeScript to 5.5.4, and refactor HttpClient usage in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app.routes';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { GameComponent } from './components/game/game.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HelpModalComponent } from './components/help-modal/help-modal.component';
 import { SettingsModalComponent } from './components/settings-modal/settings-modal.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
@@ -17,28 +17,21 @@ export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ErrorPageComponent,
-    HomePageComponent,
-    GameComponent,
-    HelpModalComponent,
-    SettingsModalComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(AppRoutes),
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ErrorPageComponent,
+        HomePageComponent,
+        GameComponent,
+        HelpModalComponent,
+        SettingsModalComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        RouterModule.forRoot(AppRoutes),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
